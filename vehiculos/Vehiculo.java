b50840ab50152a0247f797cda57872ecf35ec497
xjose999 <josejoestar80@gmail.com>
2026-04-14 23:01:48 -0500

    Ejercicio 2

---
+++ vehiculos/Vehiculo.java
@@ -0,0 +1,10 @@
+package vehiculos;
+
+public class Vehiculo {
+    // 1. Propiedad con acceso de paquete (default)
+    String tipo;
+
+    public Vehiculo(String tipo) {
+        this.tipo = tipo;
+    }
+}
